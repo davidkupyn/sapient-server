fccf4db97d1cff35b3998008a3b23e2fe2defd66
michal34 <michalcyran2000@gmail.com>
2023-09-30 19:48:56 +0200

    created test route

--- main.py
+++ main.py
@@ -1,13 +1,38 @@
 from flask import Flask, jsonify
+import psycopg2
+from dotenv import load_dotenv
 import os
 
 app = Flask(__name__)
 
+database = os.getenv("DATABASE")
+user = os.getenv("USER")
+password = os.getenv("PASSWORD")
+host = os.getenv("HOST")
+port = os.getenv("PORT")
 
 @app.route('/')
 def index():
     return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
 
+@app.route('/search', methods=['GET'])
+def search():
+    conn = psycopg2.connect(database=database, 
+                            user=user,
+                            password=password, 
+                            host=host, 
+                            port=port)
+    
+    cur = conn.cursor()
+
+    cur.execute('SELECT * FROM test')
+  
+    data = cur.fetchall()
+  
+    cur.close()
+    conn.close()
+    
+    return data
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
