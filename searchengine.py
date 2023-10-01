81a277e8a6f1a42d24b1dd582946010fb00478e0
Rexoo <herricux99@gmail.com>
2023-10-01 03:14:37 +0200

    Search Engine v1.0 test

---
+++ searchengine.py
@@ -0,0 +1,45 @@
+import openai
+import requests
+
+api_key = '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'
+
+openai.api_key = api_key
+
+
+def fetch_school_data():
+    url = 'https://sapient-api.kupyn.dev/schools'
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.text
+    else:
+        return None
+
+
+def chatgpt_search(query, school_data):
+    prompt = f"Search for: {query}\nSchool Data: {school_data}\n"
+
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        max_tokens=50,
+    )
+
+    return response.choices[0].text.strip()
+
+
+if __name__ == "__main__":
+    school_data = fetch_school_data()
+    if school_data is None:
+        print("Failed to fetch school data. Check your Flask app URL.")
+    else:
+        print("School Data Fetched Successfully.")
+
+    while True:
+        query = input("Enter your search query (or 'exit' to quit): ")
+
+        if query.lower() == 'exit':
+            break
+
+        result = chatgpt_search(query, school_data)
+        print("Search Result:")
+        print(result)
