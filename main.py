2113684e4b1dc6d2c89bf12ba26f2136a59eb893
LateForever <albertgrzegrzolka124@gmail.com>
2023-10-01 09:59:43 +0200

    fixed performance in search

--- main.py
+++ main.py
@@ -64,57 +64,63 @@ def ai_api():
     if q == None:
         return jsonify({"error": "No query provided"})
 
-    gpt_prompt_schema = "Przetransformuj odpowiedź użytkownika, tak aby była zdatna do użycia w wyszukiwarce. Musisz ocenić zdolności użytkownika, jego mocne i słabe strony oraz to, co opowie o sobie, aby znaleźć dla niego idealny kierunek studiów. Jeżeli wspomni coś o lokalizacji, uwzględnij to. Prompt do wyszukiwarki, który masz za zadanie napisać, musi być krótki i zawierać kluczowe słowa. Przykład: Użytkownik napisał: 'Uwielbiam informatykę! Od urodzenia jestem umysłem ścisłym, jednak nie mogę się zdecydować nad studiami. Chciałbym zostać w moim rodzinnym mieście - Warszawie.' Twoja odpowiedź powinna brzmieć: 'Informatyka Warszawa.' PAMIĘTAJ, ABY OGRANICZYĆ SIĘ DO KILKU (max 3) KLUCZOWYCH SŁÓW KTÓRE POMOGĄ OSOBIE OPISUJĄCEJ ZNALEŹĆ IDEALNY PROFIL STUDENCKI!!! Twój opis do analizy to: "
-    gpt_prompt_end = "MAKSYMALNA ILOŚĆ SŁÓW KLUCZOWYCH DO UŻYCIA TO 3. ODWOŁUJ SIĘ JEDYNIE DO PRZYSZYŁYCH STUDIÓW A NIE PRACY, unikaj przecinków i kropek, pamiętaj o podaniu lokalizacji jeśli użytkownik ją uwzględnił."
-
-    gpt_prompt = gpt_prompt_schema + q + gpt_prompt_end
-
-    openai.api_key = os.getenv("OPENAI_API_KEY")
-    messages = [{"role": "user", "content": gpt_prompt}] 
-    chat = openai.ChatCompletion.create( 
-        model="gpt-3.5-turbo", 
-        messages=messages, 
-        temperature=0.1, 
-    )
-
-    response = chat["choices"][0]["message"]["content"]
-
-    conn = psycopg2.connect(database=database, 
+    if q.__len__() < 12:
+        conn = psycopg2.connect(database=database, 
                             user=user,
                             password=password, 
                             host=host, 
                             port=port)
     
-    cur = conn.cursor()
-    
-    transformed_response = response.replace(" ", " & ")
+        cur = conn.cursor()
 
-    cur.execute(f"SELECT id, name, city, LEFT(school_description, 100) AS school_description, voivodeship FROM schools WHERE ts_vector @@ to_tsquery('{transformed_response}')")
-    data = cur.fetchall()
-
-    if len(data) == 0:
-
-        ts_vector_format = response.split(" ")
-        ts_vector_format[0] = "(" + ts_vector_format[0]
-        ts_vector_format[1] = " | " + ts_vector_format[1] + ") & "
-
-        ts_vector = "".join(ts_vector_format)
-
-        print(ts_vector)
-
-        cur.execute(f"SELECT id, name, city, LEFT(school_description, 100) AS school_description, voivodeship FROM schools WHERE ts_vector @@ to_tsquery('{ts_vector}')")
+        transformed_response = q.replace(" ", " & ")
+        cur.execute(f"SELECT id, name, city, LEFT(school_description, 100) AS school_description, voivodeship FROM schools WHERE ts_vector @@ to_tsquery('{transformed_response}')")
         data = cur.fetchall()
 
-        print(data)
+        if len(data) == 0:
+            return jsonify({"message": "Nie znaleziono szkół spełniających twoje kryteria. Spróbuj ponownie."})
 
-    cur.close()
-    conn.close()
+        cur.close()
+        conn.close()
 
-    # return voivodeship list
-    # return cities list
-    # return 
+        return jsonify(data)
+    
+    else:
+        gpt_prompt_schema = "NA SAMYM POCZĄTKU PRZETŁUMACZ PODANY TEKST NA JĘZYK POLSKI następnie Przetransformuj odpowiedź użytkownika, tak aby była zdatna do użycia w wyszukiwarce. Musisz ocenić zdolności użytkownika, jego mocne i słabe strony oraz to, co opowie o sobie, aby znaleźć dla niego idealny kierunek studiów. Jeżeli wspomni coś o lokalizacji, uwzględnij to. Prompt do wyszukiwarki, który masz za zadanie napisać, musi być krótki i zawierać kluczowe słowa. Przykład: Użytkownik napisał: 'Uwielbiam informatykę! Od urodzenia jestem umysłem ścisłym, jednak nie mogę się zdecydować nad studiami. Chciałbym zostać w moim rodzinnym mieście - Warszawie.' Twoja odpowiedź powinna brzmieć: 'Informatyka Warszawa.' PAMIĘTAJ, ABY OGRANICZYĆ SIĘ DO KILKU (max 3) KLUCZOWYCH SŁÓW KTÓRE POMOGĄ OSOBIE OPISUJĄCEJ ZNALEŹĆ IDEALNY PROFIL STUDENCKI!!! Twój opis do analizy to: "
+        gpt_prompt_end = "MAKSYMALNA ILOŚĆ SŁÓW KLUCZOWYCH DO UŻYCIA TO 3. ODWOŁUJ SIĘ JEDYNIE DO PRZYSZYŁYCH STUDIÓW A NIE PRACY, unikaj przecinków i kropek, pamiętaj o podaniu lokalizacji jeśli użytkownik ją uwzględnił."
 
-    return jsonify(data)
+        gpt_prompt = gpt_prompt_schema + q + gpt_prompt_end
+
+        openai.api_key = os.getenv("OPENAI_API_KEY")
+        messages = [{"role": "user", "content": gpt_prompt}] 
+        chat = openai.ChatCompletion.create( 
+            model="gpt-3.5-turbo", 
+            messages=messages, 
+            temperature=0.1, 
+        )
+
+        response = chat["choices"][0]["message"]["content"]
+
+        conn = psycopg2.connect(database=database, 
+                                user=user,
+                                password=password, 
+                                host=host, 
+                                port=port)
+        
+        cur = conn.cursor()
+    
+        transformed_response = response.replace(" ", " & ")
+
+        cur.execute(f"SELECT id, name, city, LEFT(school_description, 100) AS school_description, voivodeship FROM schools WHERE ts_vector @@ to_tsquery('{transformed_response}')")
+        data = cur.fetchall()
+
+        if len(data) == 0:
+            return jsonify({"message": "Nie znaleziono szkół spełniających twoje kryteria. Spróbuj ponownie."})
+
+        cur.close()
+        conn.close()
+
+        return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
